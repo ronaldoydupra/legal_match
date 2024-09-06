3fb58741ef499776dda679d15916a3005c2fa5f8
Ronaldo Dupra <ronaldoydupra@yahoo.com>
2024-09-06 09:49:34 +0800

    update ami and s3 id

--- variables.tf
+++ variables.tf
@@ -10,10 +10,10 @@ variable "instance_type" {
 
 variable "ami" {
   description = "AMI for EC2"
-  default     = "ami-070f589e4b4a3fece"  # Ubuntu Server 22.04 LTS (64-bit Arm)
+  default     = "ami-0f0a689dea27d9dc8"  # Ubuntu Server 22.04 LTS (64-bit Arm)
 }
 
 variable "s3_bucket_name" {
   description = "S3 bucket name"
-  default     = "my-unique-s3-bucket"
+  default     = "lgm-2024-s3-bucket"
 }
