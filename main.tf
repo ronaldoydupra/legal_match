9045a12d3cd8fc0ccaf9e52e85c72d0bbc0d82f9
Ronald <ronaldoydupra@yahoo.com>
2025-01-29 08:51:54 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,13 @@ provider "aws" {
   region = var.aws_region
 }
 
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = "us-west-2" # Adjust region as needed
+}
+
+
 terraform {
   backend "local" {
     path = "/var/jenkins_home/terraform_state/terraform.tfstate"
