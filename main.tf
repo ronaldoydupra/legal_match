ea2c2e7045d4a4ec738fbec5ca522465f30e7aee
Ronald <ronaldoydupra@yahoo.com>
2025-01-29 08:54:04 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,7 @@
 provider "aws" {
-  region = var.aws_region
-}
-
-provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
-  region     = "us-west-2" # Adjust region as needed
+  region = var.aws_region
 }
 
 
